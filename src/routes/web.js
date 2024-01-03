653bf246d9e059d20b30cf3d28bbe382b15da10e
LVQT <levietquocthinh@gmail.com>
2024-01-03 14:52:47 +0700

    MVC model

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,4 +1,6 @@
 import express from "express";
+import homeController from "../controller/homeController";
+
 
 const router = express.Router();
 
@@ -7,10 +9,11 @@ const router = express.Router();
  * @param {*} app -- express app  
  */
 
+
+
 const initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("hello world");
-    })
+    router.get("/", homeController.handleHelloWorld)
+    router.get("/user", homeController.handleUserPage)
     return app.use("/", router);
 }
 export default initWebRoutes;
