af067877e00da65d1f7e0d6c67ee45d463372f11
LVQT <levietquocthinh@gmail.com>
2024-01-10 23:56:44 +0700

    connect DB

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,13 @@
+// Get the client
+import mysql from 'mysql2';
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt',
+});
+
 const handleHelloWorld = (req, res) => {
 
     return res.render("home.ejs")
@@ -5,7 +15,27 @@ const handleHelloWorld = (req, res) => {
 const handleUserPage = (req, res) => {
     return res.render("user.ejs")
 }
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
+    connection.query(
+        'INSERT INTO users (email, password, username) VALUES (?, ?, ?)',
+        [email, password, username],
+        function (err, results, fields) {
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Error creating user");
+            }
+            console.log("User created successfully");
+            return res.send("User created successfully");
+        }
+    );
+
+    return res.send("create ser")
+}
 module.exports = {
     handleHelloWorld,
-    handleUserPage
+    handleUserPage,
+    handleCreateNewUser
 }
--- src/server.js
+++ src/server.js
@@ -2,10 +2,16 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
+import bodyParser from "body-parser";
 // nếu port không định nghĩa thì sẽ chuyển sang 8080 
 const PORT = process.env.PORT || 8080;
 const app = express();
+//config body parser 
+// support parsing of application/json type post data
+app.use(bodyParser.json());
 
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 // config view engine 
 configViewEngine(app);
 // init web routes 
