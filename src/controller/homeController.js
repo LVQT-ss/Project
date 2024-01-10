f9388639f5a50133b4bd10ed3897acc851f7ca6d
LVQT <levietquocthinh@gmail.com>
2024-01-11 00:22:16 +0700

    create user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -5,8 +5,9 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'jwt',
+    database: 'jwt'
 });
+
 
 const handleHelloWorld = (req, res) => {
 
@@ -19,16 +20,11 @@ const handleCreateNewUser = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
     let username = req.body.username;
+
     connection.query(
-        'INSERT INTO users (email, password, username) VALUES (?, ?, ?)',
-        [email, password, username],
+        'INSERT INTO users (email,password,username) VALUES (?,?,?)', [email, password, username],
         function (err, results, fields) {
-            if (err) {
-                console.error(err);
-                return res.status(500).send("Error creating user");
-            }
-            console.log("User created successfully");
-            return res.send("User created successfully");
+            console.log(err); // fields contains extra meta data about results, if available
         }
     );
 
